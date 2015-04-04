8497a96110a1d05b3a88bbf2a4a78cf8e4823b1e
Jialin Li <jialin_li@utexas.edu>
2015-04-04 01:14:22 -0500

    Update dna.py

--- dna.py
+++ dna.py
@@ -8,7 +8,7 @@ class dna:
         self.weights=[]
         for c in range(0, dna.weights_size):
             #randomly assign weights in range -5.0 to 5.0
-            self.weights.append(random.uniform(-5.0, 5.0))
+            self.weights.append(random.uniform(-3.0, 3.0))
 
     def mutate(self):
         # create a new dna
@@ -16,7 +16,7 @@ class dna:
         # copy paren'ts 
         for x in range(0, dna.weights_size):
             child.weights.append(self.weights[x])
-        index_to_mutate = random.randint(0, dna.chromosome_size)
+        index_to_mutate = random.randint(0, dna.weights_size)
         # randomly pick a percentage to increase or decrease by
         percentage = random.uniform(0.01, 0.25)
         # randomly pick to the mutation factor to be positive or negative
@@ -24,16 +24,16 @@ class dna:
             percentage = 1 + percentage
         else:
             percentage = 1 - percentage
-        child[index_to_mutate] *= percentage
+        child.weights[index_to_mutate] =  child.weights[index_to_mutate] * percentage
         return child
 
     def crossover(self, other):
         # do crossover
         child = dna()
         for x in range(0, dna.weights_size):
-            if random.random() > 0.5
+            if random.random() > 0.5:
                 child.weights[x] = self.weights[x]
-            else
+            else:
                 child.weights[x] = other.weights[x]
         return child
     
@@ -44,24 +44,26 @@ class dna:
 
     def evaluate(self):
         neuron = self.copyNeuron()
-        self.fitness = evaluate_neuron(neuron)
+        self.fitness = neuron.evaluate_neuron()
 
 
 class neuron:
 
     def __init__(self):
-        self.weights = [random.uniform(-2.0, 2.0), random.uniform(-2.0, 2.0), random.uniform(-2.0, 2.0)]
+        self.weights = [random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)]
 
 
     def process(self, sensors):
         output = 0
         for x in range(0, dna.weights_size):
             output += sensors[x] * self.weights[x]
-        if(output > 0):
+        if output > 0:
+            #print 'jump'
             return 1.0
         else:
+            #print 'not do anything'
             return 0.0
 
     def evaluate_neuron(self):
-        return jump.game(self, False)
+        return jumpmain.game_function(self)
 
