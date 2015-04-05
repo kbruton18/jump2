35e26e73c8298b1c27e855c8e3a335ee7faef919
Jialin Li <jialin_li@utexas.edu>
2015-04-05 00:47:39 -0500

    Update jump_evolve.py

--- jump_evolve.py
+++ jump_evolve.py
@@ -8,6 +8,7 @@ from pygame.locals import *
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+PRETTY_BLUE = (135,206,250)
 
 
 pygame.init()
@@ -38,7 +39,7 @@ def game_function (ann, display=False):
     obstacle_one_vx = 8
 
      # obstacle 2
-    obstacle_two_x = obstacle_one_x + 300
+    obstacle_two_x = obstacle_one_x + 400
     obstacle_two_y = 440
     obstacle_two_vx = 8
 
@@ -55,13 +56,11 @@ def game_function (ann, display=False):
 
     while game and score < 300:
         score += 0.123
-        #min_rel_x = min(abs(player_x - obstacle_one_x), abs(player_x - obstacle_two_x)) / 350
-        #min_rel_y = min(abs(player_y - obstacle_one_y), abs(player_y - obstacle_two_y)) / 150
-        rel_x = (obstacle_one_x - player_x)/800.0
-        rel_y = (player_y - obstacle_one_y)/1.0
-        abs_x = obstacle_one_x / 800.0
+        rel_x = min(abs(player_x - obstacle_one_x), abs(player_x - obstacle_two_x))
+        abs_x = min(obstacle_one_x, obstacle_two_x)
+        #rel_x = (obstacle_one_x - player_x)/800.0
+        #abs_x = obstacle_one_x / 800.0
 
-        #sensors = [rel_x, abs_x, 1.0]
         sensors = [rel_x,abs_x, 1.0]
         output = ann.process(sensors)
 
@@ -73,7 +72,7 @@ def game_function (ann, display=False):
         if jump_cycle and player_y > top_y and not reached_top: #if it has not reached the ceiling, increase velocity to go up
             player_vy = -10 + player_ay
         elif jump_cycle and reached_top: #if it's reaching the ceiling, start going down
-            player_vy = 8 + player_ay
+            player_vy = 10 + player_ay
 
         # update player y position
         if(player_y <= bottom_y and player_y >= top_y): #if it's in bound, update it
@@ -90,40 +89,35 @@ def game_function (ann, display=False):
          # create rectangle around circle and move the rectangle with the circle
         playerRect = pygame.Rect(player_x - 25, player_y - 25, 50, 50)
         obstacle_one = pygame.Rect(obstacle_one_x - 23, obstacle_one_y - 23, 45, 45)
-        #obstacle_two = pygame.Rect(obstacle_two_x - 23, obstacle_two_y - 23, 45, 45)
+        obstacle_two = pygame.Rect(obstacle_two_x - 23, obstacle_two_y - 23, 45, 45)
 
         # update obstacle x position
         obstacle_one_x -= obstacle_one_vx
         if(obstacle_one_x < 5):
-            #obstacle_one_x = 780
-            obstacle_one_x = random.randint(450, 780)
+            obstacle_one_x = obstacle_two_x + random.randint(400, 500)
 
-        # obstacle two, update position
-       # obstacle_two_x -= obstacle_two_vx
-       # if(obstacle_two_x < 5):
-         #   number = random.randint(400, 500)
-         #   obstacle_two_x = obstacle_one_x + number
+
+        #obstacle two, update position
+        obstacle_two_x -= obstacle_two_vx
+        if(obstacle_two_x < 5):
+            number = random.randint(400, 500)
+            obstacle_two_x = obstacle_one_x + number
 
         # check if there is collision object 1
         if playerRect.colliderect(obstacle_one):
             game = False
 
         # check if there is collision object 2
-#        if playerRect.colliderect(obstacle_two):
- #           game = False
+        if playerRect.colliderect(obstacle_two):
+            game = False
 
         if display:
             screen.fill(BLACK)
             pygame.draw.circle(screen, WHITE, (player_x, player_y), 25, 0) #draw the player
             pygame.draw.rect(screen, RED, obstacle_one, 0) # draw the obstacle
-            #pygame.draw.rect(screen, RED, obstacle_two, 0)
+            pygame.draw.rect(screen, PRETTY_BLUE, obstacle_two, 0)
             pygame.display.update()
             clock.tick(30)
-
-        #if(not game):
-         #   pygame.quit()
-
-            #pygame.display.flip()
 
     return score
 
@@ -136,4 +130,4 @@ if (__name__=='__main__'):
     pygame.quit()
     sys.exit()
 
-#jump().game()
+
